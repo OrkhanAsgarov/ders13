330ce25d60766cbad88377d59fe48d5f4a6fbd8b
OrkhanAsgarov <105534845+OrkhanAsgarov@users.noreply.github.com>
2022-08-07 20:20:16 +0300

    karmasik icice route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,14 @@ function App() {
               <Route index element={<AnaEkran />} />
               <Route path="ekran1" element={<Ekran1 />} />
               <Route path="ekran2" element={<Ekran2 />} />
+
+              <Route path="kategori/">
+                <Route path="kadin" element={<Ekran1 />} />
+                <Route path="erkek" element={<Ekran2 />} />
+              </Route>
+
               <Route path="*" element={<Bulunamadi />} />
+              
             </Route>
           </Routes>
 
